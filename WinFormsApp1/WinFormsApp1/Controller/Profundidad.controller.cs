c25deebdc76ef277616320bd7bc08241b5d5119a
Ander <Anderson_ortiz05@hotmail.com>
2023-08-10 10:44:55 -0400

    Logica de la app

---
+++ WinFormsApp1/WinFormsApp1/Controller/Profundidad.controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1.Models.Profundidad;
+
+namespace WinFormsApp1.Controller
+{
+    public class ProfundidadCtrl
+    {
+        public List<Profundidad> getProfundidad()
+        {
+            List<Profundidad> profundidad = new List<Profundidad>();     
+            using(var context = new Context())
+            {
+                profundidad = context.profundidad.ToList();
+            }
+            return profundidad;
+        }
+        public Profundidad getProfundidadById(int id)
+        {
+            Profundidad profundidad = new Profundidad();
+            using(var context = new Context())
+            {
+                profundidad = context.profundidad.SingleOrDefault(b=>b.id == id); 
+            }
+            return profundidad;
+        }
+        public bool insertProfundidad(Profundidad profundidad)
+        {
+            try 
+            {
+                using (var context = new Context())
+                {
+                    context.profundidad.Add(profundidad);
+                    context.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+       
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Models/Riesgo/Riesgo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WinFormsApp1.Models.Riesgo
+{
+    public class Riesgo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public String active { get; set; }
+        public String risk { get; set; }
+        public String damage { get; set; }
+        public String analist { get; set; }
+
+    }
+}
